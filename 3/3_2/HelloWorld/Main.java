3cdf1febe9aa7601bc5559c0fe6dff27b2494e49
Tharushi <ththarushi3@gmail.com>
2023-09-10 00:38:40 +0530

    Java: 3_1, 3_2, 3_3

---
+++ 3/3_2/HelloWorld/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello from Java!");
+        System.out.println(args[0]);
+    }
+}
+
+// PS G:\Repositories\java-essential-training\3\3_2\HelloWorld> java Main My Argument
+// Hello from Java!
+// My
+
+// PS G:\Repositories\java-essential-training\3\3_2\HelloWorld> java Main "My Argument"
+// Hello from Java!
+// My Argument
